39f851a4473d31982782fb357d4306420d47e35c
Sojan James <sojan.james@gmail.com>
2020-12-13 20:07:53 +0530

    Adding server stub
    Added server stubs. I still haven't figured out how to glue
    it all together.

--- src/server.rs
+++ src/server.rs
@@ -7,7 +7,30 @@ use crate::common::TxHandle;
 use crate::context::RuntimeEntityReference;
 use crate::error::InitceptionServerError;
 
+use crate::application::src_gen::application_interface_ttrpc;
+use crate::application::src_gen::application_interface;
 
+use ttrpc::r#async::Server;
+use ttrpc;
+use async_trait::async_trait;
+
+struct ServiceManager 
+{
+    spawnref: RuntimeEntityReference, // reference to the runtime entity for this server
+}
+
+#[async_trait]
+impl application_interface_ttrpc::ApplicationManager for ServiceManager {
+
+    async fn heartbeat(&self, _ctx: &::ttrpc::r#async::TtrpcContext, _req: application_interface::HeartbeatRequest) -> ::ttrpc::Result<application_interface::HeartbeatReply> {
+        
+        Err(ttrpc::Error::RpcStatus(::ttrpc::get_status(::ttrpc::Code::NOT_FOUND, "/grpc.ApplicationManager/heartbeat is not supported".to_string())))
+    }
+    async fn statechanged(&self, _ctx: &::ttrpc::r#async::TtrpcContext, _req: application_interface::StateChangedRequest) -> ::ttrpc::Result<application_interface::StateChangedReply> {
+
+        Err(ttrpc::Error::RpcStatus(::ttrpc::get_status(::ttrpc::Code::NOT_FOUND, "/grpc.ApplicationManager/statechanged is not supported".to_string())))
+    }
+}
 
 /// spawn a server to handle a service. The tx handle is used to send back messages to
 /// the main task. The stream is used to communicate with the process.
