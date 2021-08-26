18aab60d2719f7a1add9cae0047756f19b5fd7a9
Sojan James <sojan.james@gmail.com>
2021-08-26 16:43:41 +0530

    Added the LifecycleControl interface

--- libinitception/src/app_manager_interface.rs
+++ libinitception/src/app_manager_interface.rs
@@ -34,37 +34,88 @@ pub trait ApplicationControl {
     /// Stop all activities. The process may get killed after this
     /// call is acknowledged.
     async fn stop(&self) -> Result<(), ApplicationManagerError>;
+    /// A property that the application is interested in has changed. The 
+    /// property key and value is sent.
     async fn on_property_changed(&self, key: String, value: String) -> Result<(), ApplicationManagerError>;
+    /// An event has been sent to the application.
     async fn handle_event(&self, key: String, value: String) -> Result<(), ApplicationManagerError>;
 }
 
-/*
 
-#[service_impl(ApplicationControl)]
-pub struct ApplicationControlImpl {
-
+#[derive(Error, Debug, Serialize, Deserialize)]
+pub enum  LifecycleControlError {
+    #[error("Unknown Error")]
+    Unknown,
 }
 
+#[derive(Serialize, Deserialize)]
+pub enum ApplicationStatus {
+    Unknown,
+    Stopped,
+    Running,
+    Paused,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum ShutdownType {
+    /// Normal shutdown
+    Normal,
+    /// Fast shutdown
+    Fast,
+    /// Super fast shutdown! This is an emergency
+    Emergency,
+}
+
+/// The LifecycleControl interface is provided by initception. The Lifecycle manager
+/// application uses this interface to manage the lifecycle of the entire system.
+/// The lifecycle manager is a privileged application that gets access to this
+/// interface at startup via an anonymous UnixDomain socket.
+#[service(name("dev.sabaton.LifecycleControl"))]
 #[async_trait]
-impl ApplicationControl for ApplicationControlImpl {
-    
-    async fn pause(&self) -> Result<(), ApplicationManagerError> {
-        Ok(())
-    }
-    async fn resume(&self) -> Result<(), ApplicationManagerError> {
-        Ok(())
-    }
-    async fn stop(&self) -> Result<(), ApplicationManagerError> {
-        Ok(())
-    }
+pub trait LifecycleControl {
+    /// Return the list of all applications in the system.
+    async fn get_applications(&self) -> Result<Vec<String>,LifecycleControlError>;
+    /// Return the status of an application. An application is identified by name.
+    async fn get_application_status(&self,name: String) -> Result<ApplicationStatus,LifecycleControlError>;
+    /// Start the application. 
+    /// # Returns
+    /// On success, the function returns the time it took to start the application
+    async fn start_application(&self,name: String) -> Result<std::time::Duration,LifecycleControlError>;
+    /// Restart the application. This will force a shutdown and then an restart.
+    /// # Returns
+    /// On success, the function returns the time it took to restart the application
+    async fn restart_application(&self,name: String) -> Result<std::time::Duration,LifecycleControlError>;
 
-    async fn handle_event(&self, key: String, value: String) -> Result<(), ApplicationManagerError> {
-        Ok(())
-    }
+    /// Pause the application. 
+    /// # Returns
+    /// On success, the function returns the time it took to pause the application
+    async fn pause_application(&self,name: String) -> Result<std::time::Duration,LifecycleControlError>;
 
-    async fn on_property_changed(&self, key: String, value: String) -> Result<(), ApplicationManagerError> {
-        Ok(())
-    }
-    
+    /// Resume the application. 
+    /// # Returns
+    /// On success, the function returns the time it took to resume the application
+    async fn resume_application(&self,name: String) -> Result<std::time::Duration,LifecycleControlError>;
+
+    /// Stop the application. 
+    /// # Returns
+    /// On success, the function returns the time it took to stop the application
+    async fn stop_application(&self,name: String) -> Result<std::time::Duration,LifecycleControlError>;
+
+    /// Prepare the system for freeze. 
+    /// # Returns
+    /// On success, the system is ready to go to sleep
+    async fn prepare_system_freeze(&self, shutdown_type: ShutdownType) -> Result<(),LifecycleControlError>;
+
+    /// Prepare the system for shutdown. 
+    /// # Returns
+    /// On success, the system is ready to be shutdown
+    async fn prepare_system_shutdown(&self, shutdown_type: ShutdownType) -> Result<(),LifecycleControlError>;
+
+    /// Shutdown the system
+    /// # Returns
+    /// TBD
+    async fn shutdown_system(&self, shutdown_type: ShutdownType) -> Result<(),LifecycleControlError>;
+
 }
-*/
+
+
