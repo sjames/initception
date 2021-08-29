08effbdf4004132414739aae7b94ebda6f92c488
Sojan James <sojan.james@gmail.com>
2021-08-29 17:43:18 +0530

    Removing remnants of ttrpc

--- libinitception/src/lib.rs
+++ libinitception/src/lib.rs
@@ -2,14 +2,8 @@ pub mod app;
 pub mod application_events;
 pub mod config;
 pub mod initrc;
-//mod src_gen;
 pub mod app_interface;
 
-//pub use src_gen::application_interface;
-//pub use src_gen::application_interface_ttrpc::ApplicationServiceClient;
-//pub use src_gen::application_interface_ttrpc::{
-//    create_application_manager, create_lifecycle_server, ApplicationManager, LifecycleServer,
-//};
 
 #[cfg(test)]
 mod tests {
--- libinitception/src/src_gen/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod application_interface;
-pub mod application_interface_ttrpc;
